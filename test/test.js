34e80150773bd25bd6b872023059781adaedcb20
Kai Davenport <kaiyadavenport@gmail.com>
2014-04-21 21:40:22 +0000

    test for external add

--- test/test.js
+++ test/test.js
@@ -130,3 +130,42 @@ test('finish even with no streams', function(t) {
 
   source.pipe(trigger).pipe(sink)
 })
+
+
+test('allow arbritrary stream to be added outside the scope', function(t) {
+
+  // create a stream for a single letter
+  function createLetterStream(letter){
+    return from.obj([1,2,3].map(function(num){
+      return letter + ':' + num
+    }))
+  }
+
+  var source = from(['a', 'b', 'c'])
+
+  var trigger = cascade(function(chunk, add, next){
+    add(createLetterStream(chunk))
+    next()
+  })
+
+  var arr = []
+  var sink = through(function(chunk, enc, cb){
+    arr.push(chunk)
+    if(chunk.toString()=='b:1'){
+      trigger.add(createLetterStream('d'))
+    }
+    cb()
+  }, function(){
+    t.equal(arr.length, 12)
+    arr = arr.map(function(a){
+      return a.toString()
+    })
+    t.equal(arr[2], 'a:3')
+    t.equal(arr[6], 'd:1')
+
+
+    t.end()
+  })
+
+  source.pipe(trigger).pipe(sink)
+})

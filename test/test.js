a364c39fe99f17c4e53073e3adf2ca1a28405aa0
Kai Davenport <kaiyadavenport@gmail.com>
2014-04-22 10:41:28 +0000

    test for delayed streams not ending the cascade before they have finished

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var test = require('tape')
 
 var through = require('through2')
 var from = require('from2-array')
+var fnfrom = require('from2')
 var cascade = require('../')
 
 
@@ -163,6 +164,53 @@ test('allow arbritrary stream to be added outside the scope', function(t) {
     t.equal(arr[2], 'a:3')
     t.equal(arr[6], 'd:1')
 
+
+    t.end()
+  })
+
+  source.pipe(trigger).pipe(sink)
+})
+
+
+test('wait for inner streams to finish before triggering the end', function(t) {
+
+  function createDelayStream(letter){
+    var counter = 0;
+    return fnfrom(function(size, next){
+      setTimeout(function(){
+        counter++
+        if(counter<=3){
+          next(null, letter + counter)
+        }
+        else{
+          next()
+        }  
+      },100)
+      
+    })
+  }
+
+  var source = from(['a', 'b', 'c'])
+
+  var trigger = cascade(function(chunk, add, next){
+    add(createDelayStream(chunk))
+    next()
+  })
+
+  var arr = []
+  var sink = through(function(chunk, enc, cb){
+    arr.push(chunk)
+    cb()
+  }, function(){
+    
+    arr = arr.map(function(a){
+      return a.toString()
+    })
+
+    console.dir(arr);
+
+    t.equal(arr.length, 12)
+    
 
     t.end()
   })
